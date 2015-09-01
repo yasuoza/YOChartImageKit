6aad4605c3647d2121257b8d9bf58ae69d21c1d7
Yasuharu Ozaki <yasuharu.ozaki@gmail.com>
2015-09-01 13:08:51 +0900

    Use common header file

---
+++ Source/YOGraphImageKit/YOChartImageKit.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for YOChartImageKit.
+FOUNDATION_EXPORT double YOChartImageKitVersionNumber;
+
+//! Project version string for YOChartImageKit.
+FOUNDATION_EXPORT const unsigned char YOChartImageKitVersionString[];
+
+#import <YOChartImageKit/YODonutChartImage.h>
+#import <YOChartImageKit/YOBarChartImage.h>
+#import <YOChartImageKit/YOLineChartImage.h>
